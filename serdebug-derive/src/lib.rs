6e7f956e15415e0e50e3837d19b947af41e1059c
Ingvar Stepanyan <me@rreverser.com>
2018-02-03 15:22:57 +0000

    Add `Self: Serialize` predicate

--- serdebug-derive/src/lib.rs
+++ serdebug-derive/src/lib.rs
@@ -7,14 +7,32 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(SerDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let mut ast: syn::DeriveInput = syn::parse(input).unwrap();
 
     let name = &ast.ident;
 
-    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+    let mut predicates_tokens = quote::Tokens::default();
+
+    let empty_or_trailing;
+
+    if let Some(where_clause) = ast.generics.where_clause.take() {
+        let predicates = where_clause.predicates;
+        empty_or_trailing = predicates.empty_or_trailing();
+        predicates_tokens.append_all(predicates);
+    } else {
+        empty_or_trailing = true;
+    };
+
+    if empty_or_trailing {
+        predicates_tokens.append_all(quote!(,));
+    }
+
+    predicates_tokens.append_all(quote!(Self: ::serde::Serialize));
+
+    let (impl_generics, ty_generics, _) = ast.generics.split_for_impl();
 
     let tokens = quote! {
-        impl #impl_generics ::std::fmt::Debug for #name #ty_generics #where_clause {
+        impl #impl_generics ::std::fmt::Debug for #name #ty_generics where #predicates_tokens {
             fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
                 ::std::fmt::Debug::fmt(&::serdebug::Serialize(self), f)
             }
