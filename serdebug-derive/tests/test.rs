d7f233e6e2233dba3ce1d723aab18131b7a3a830
Ingvar Stepanyan <me@rreverser.com>
2018-07-29 14:26:58 +0100

    Add more tests

--- serdebug-derive/tests/test.rs
+++ serdebug-derive/tests/test.rs
@@ -1,42 +1,73 @@
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate serdebug;
-#[macro_use] extern crate serdebug_derive;
+#[macro_use]
+extern crate serdebug_derive;
 
-macro_rules! struct_tests {
-	(@struct ( $($payload:tt)* )) => {
-		#[derive(Serialize, SerDebug, Default)]
-		pub struct Test ( $($payload)* );
+macro_rules! test {
+	(@decl $(# $attr:tt)* struct { $($payload:tt)* }) => {
+		#[derive(Default)]
+		#[allow(dead_code)]
+		$(# $attr)*
+		pub struct Test { $($payload)* }
 	};
 
-	(@struct $payload:tt) => {
-		#[derive(Serialize, SerDebug, Default)]
-		pub struct Test $payload
+	(@decl $(# $attr:tt)* struct $($payload:tt)*) => {
+		#[derive(Default)]
+		#[allow(dead_code)]
+		$(# $attr)*
+		pub struct Test $($payload)*;
 	};
 
-	($($payload:tt)*) => {
-		$({
-			mod serdebug {
-				struct_tests!(@struct $payload);
+	(@decl $(# $attr:tt)* enum $($payload:tt)*) => {
+		$(# $attr)*
+		pub enum Test {
+			#[allow(dead_code)]
+			Variant $($payload)*
+		}
+
+		impl Default for Test {
+			fn default() -> Self {
+				test!(@decl struct $($payload)*);
+
+				unsafe {
+					::std::mem::transmute(Test::default())
+				}
+			}
+		}
+	};
+
+	(@kind $name:ident $kind:tt $($payload:tt)*) => {
+		#[test]
+		fn $name() {
+			mod lhs {
+				test!(@decl #[derive(Serialize, SerDebug)] $kind $($payload)*);
 			}
 
-			mod debug {
-				struct_tests!(@struct $payload);
+			mod rhs {
+				test!(@decl #[derive(Debug)] $kind $($payload)*);
 			}
 
-			assert_eq!(format!("{:?}", serdebug::Test::default()), format!("{:?}", debug::Test::default()));
-			assert_eq!(format!("{:#?}", serdebug::Test::default()), format!("{:#?}", debug::Test::default()));
-		})*
+			let lhs = lhs::Test::default();
+			let rhs = rhs::Test::default();
+
+			assert_eq!(format!("{:#?}", lhs), format!("{:#?}", rhs));
+			assert_eq!(format!("{:?}", lhs), format!("{:?}", rhs));
+		}
+	};
+
+	($name:ident $($payload:tt)*) => {
+		mod $name {
+			test!(@kind test_struct struct $($payload)*);
+			test!(@kind test_enum enum $($payload)*);
+		}
 	};
 }
 
-#[test]
-fn tests() {
-	struct_tests! {
-		{ a: u32, b: Option<f64> }
-		(u32)
-		(())
-		()
-		;
-	}
-}
+test!(named_fields { a: u32, b: Option<f64>, c: &'static str, d: () });
+test!(empty_named_fields {});
+test!(tuple_fields(u32, Option<f64>, &'static str, ()));
+test!(single_tuple_field(u32));
+test!(empty_tuple_fields());
+test!(no_fields);
