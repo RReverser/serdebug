d702523858f4e7875b603793dfe13f15cb095f77
Ingvar Stepanyan <me@rreverser.com>
2018-07-29 22:10:46 +0100

    Upgrade deps, simplify derive code

--- serdebug-derive/src/lib.rs
+++ serdebug-derive/src/lib.rs
@@ -1,5 +1,8 @@
 extern crate proc_macro;
+
+#[macro_use]
 extern crate syn;
+
 #[macro_use]
 extern crate quote;
 
@@ -7,35 +10,24 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(SerDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let mut ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let syn::DeriveInput {
+        ident,
+        mut generics,
+        ..
+    } = syn::parse(input).unwrap();
 
-    let name = &ast.ident;
+    generics
+        .make_where_clause()
+        .predicates
+        .push(parse_quote!(Self: ::serde::Serialize));
 
-    let mut predicates_tokens = quote::Tokens::default();
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    let needs_comma;
-
-    if let Some(where_clause) = ast.generics.where_clause.take() {
-        let predicates = where_clause.predicates;
-        needs_comma = !predicates.empty_or_trailing();
-        predicates_tokens.append_all(predicates);
-    } else {
-        needs_comma = false;
-    };
-
-    if needs_comma {
-        predicates_tokens.append_all(quote!(,));
-    }
-
-    let (impl_generics, ty_generics, _) = ast.generics.split_for_impl();
-
-    let tokens = quote! {
-        impl #impl_generics ::std::fmt::Debug for #name #ty_generics where #predicates_tokens Self: ::serde::Serialize {
+    (quote! {
+        impl #impl_generics ::std::fmt::Debug for #ident #ty_generics #where_clause {
             fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
                 ::std::fmt::Debug::fmt(&::serdebug::Wrapper(self), f)
             }
         }
-    };
-
-    tokens.into()
+    }).into()
 }
